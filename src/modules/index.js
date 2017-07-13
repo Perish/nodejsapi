ccc1a08694b95cfc39ebce64943758554581b130
Rubist <rubist@RubistdeMacBook-Pro.local>
2017-07-13 11:47:56 +0800

    Lesson 6, Create Post

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,7 @@
 import userRoutes from './users/user.routes';
-import { authJwt } from '../services/auth.services';
+import postRoutes from './posts/post.routes';
 
 export default app => {
   app.use('/api/v1/users', userRoutes);
-  app.use('/hello', authJwt, (req, res) => {
-    res.send('private: hello world!');
-  });
+  app.use('/api/v1/posts', postRoutes);
 };
---
+++ src/modules/posts/post.validations.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+export default {
+  createPost: {
+    body: {
+      title: Joi.string().min(3).required(),
+      text: Joi.string().min(10).required(),
+    },
+  },
+}
+;
